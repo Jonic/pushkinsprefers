76906c57fbbffcd9d2db400a6684c61574fc8229
Joshua Swift <joshuaswift@outlook.com>
2018-12-01 12:14:24 +0000

    Added random colour functionality

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 (function randomColour() {
-  let colourArr = ["#FF78D4", "#FF289F", "#80FF78"];
+  const catalog = document.getElementById("catalog");
+  const links = catalog.getElementsByTagName("ul");
 
-  let randomNum = Math.floor(Math.random() * 3) + 1;
+  let classes = [
+    "catalog_listing_lime",
+    "catalog_listing_pink",
+    "catalog_listing_pink_two"
+  ];
+
+  //Generate random number
+  let randomVal = Math.floor(Math.random() * 2);
+
+  for (i = 0; i < links.length; i++) {
+    //Replace default class with random class from array
+    links[i].classList.replace("catalog_listing", classes[randomVal]);
+  }
 })();
