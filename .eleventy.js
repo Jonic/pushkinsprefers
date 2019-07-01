c640e0a08de5cc82ab2818794d4c42b321846a7d
Jonic Linley <jonic@100yen.co.uk>
2019-07-01 16:44:28 +0100

    Standardise 11ty config

--- .eleventy.js
+++ .eleventy.js
@@ -1,16 +1,24 @@
+const filters = require('./src/filters')
+const layouts = ['application', 'zine']
 const shortcodes = require('./src/shortcodes')
+const transforms = require('./src/transforms')
 
 module.exports = config => {
-  config.addFilter('squash', require('./src/filters/squash.js'))
-  config.addFilter('dateDisplay', require('./src/filters/dateDisplay.js'))
-  config.addLayoutAlias('application', 'application.njk')
-  config.addLayoutAlias('zine', 'zine.njk')
+  Object.entries(filters).map(([filterName, filterFunction]) => {
+    config.addFilter(filterName, filterFunction)
+  })
+
+  for (let layout of layouts) {
+    config.addLayoutAlias(layout, `${layout}.njk`)
+  }
 
   Object.entries(shortcodes).map(([shortcodeName, shortcodeFunction]) => {
     config.addShortcode(shortcodeName, shortcodeFunction)
   })
 
-  config.addTransform('minifyHtml', require('./src/utils/minifyHtml.js'))
+  Object.entries(transforms).map(([transformName, transformFunction]) => {
+    config.addShortcode(transformName, transformFunction)
+  })
 
   return {
     dir: {
